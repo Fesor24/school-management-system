946feca55df155816f7c38eb65a4ef2710e5894f
Wale <onafesowale@gmail.com>
2023-12-02 04:35:24 +0100

    Course created event

---
+++ src/SMS.Domain/Events/Course/CourseCreatedEvent.cs
@@ -0,0 +1,13 @@
+﻿using SMS.Domain.Common;
+using CourseEntity = SMS.Domain.Entities.Course;
+
+namespace SMS.Domain.Events.Course;
+public class CourseCreatedEvent : BaseEvent
+{
+    public CourseCreatedEvent(CourseEntity course)
+    {
+        Course = course;
+    }
+
+    public CourseEntity Course { get; set; }
+}
