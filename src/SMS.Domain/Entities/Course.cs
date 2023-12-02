1e5b1d14d2ba8272ea0dec084679c83d728b5e8c
Wale <onafesowale@gmail.com>
2023-12-02 04:35:14 +0100

    Course entity

---
+++ src/SMS.Domain/Entities/Course.cs
@@ -0,0 +1,19 @@
+﻿using SMS.Domain.Common;
+
+namespace SMS.Domain.Entities;
+public class Course : BaseAuditableEntity
+{
+    public Course()
+    {
+        
+    }
+
+    public Course(string courseName, string courseCode)
+    {
+        CourseName = courseName;
+        CourseCode = courseCode;
+    }
+
+    public string CourseName { get; private set; }
+    public string CourseCode { get; private set; }
+}
