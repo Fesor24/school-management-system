3e858d634d613898099a3a458170f38a6e6815ee
Wale <onafesowale@gmail.com>
2023-12-02 04:34:44 +0100

    BaseEntity

---
+++ src/SMS.Domain/Common/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SMS.Domain.Common;
+public class BaseEntity
+{
+    public int Id { get; set; }
+
+    private readonly List<BaseEvent> _domainEvents = new();
+
+    [NotMapped]
+    public IReadOnlyCollection<BaseEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    public void AddDomainEvents(BaseEvent baseEvent) =>
+        _domainEvents.Add(baseEvent);
+
+    public void DeleteDomainEvents(BaseEvent baseEvent) => 
+        _domainEvents.Remove(baseEvent);
+
+    public void ClearDomainEvents() => 
+        _domainEvents.Clear();
+}
