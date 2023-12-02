0964ca89ebc8029db5164dace1554f9f1c45350e
Wale <onafesowale@gmail.com>
2023-12-02 04:35:44 +0100

    Application db context

---
+++ src/SMS.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Domain.Entities;
+
+namespace SMS.Infrastructure.Data;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Course> Courses => Set<Course>();
+}
