b03f1fa31e4c4b7a04870a28bf5ecd2ace446c61
Wale <onafesowale@gmail.com>
2023-12-03 06:54:45 +0100

    Dependency injection

---
+++ src/SMS.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SMS.Infrastructure.Data;
+
+namespace SMS.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, 
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(opt =>
+        {
+            opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        return services;
+    }
+}
