c70a85cbdf6741f7d63a2ae1a1e905a6c4cdafe0
Wale <onafesowale@gmail.com>
2023-12-03 06:50:27 +0100

    added infrastructure service

--- src/SMS.API/Program.cs
+++ src/SMS.API/Program.cs
@@ -1,4 +1,10 @@
+using SMS.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructureServices(builder.Configuration);
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
