902df06fedfbf917c0f9fb78e79f5ae00af9e535
Wale <onafesowale@gmail.com>
2023-12-03 06:53:44 +0100

    Error class

---
+++ src/SMS.Domain/Shared/Error.cs
@@ -0,0 +1,22 @@
+﻿namespace SMS.Domain.Shared;
+public class Error : IEquatable<Error>
+{
+    public static readonly Error None = new(string.Empty, string.Empty);
+    public static readonly Error NullValue = new("Error.NullValue", "The specified result value is null");
+
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; set; }
+
+    public string Message { get; set; }
+    public bool Equals(Error other)
+    {
+        return other != null && other == this;
+    }
+
+    public static implicit operator Result(Error error) => Result.Failure(error);
+}
