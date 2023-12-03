a08b76494b36b9ba02009e3d7b2dcc8813b19414
Wale <onafesowale@gmail.com>
2023-12-03 06:53:03 +0100

    Course entity

--- src/SMS.Domain/Entities/Course.cs
+++ src/SMS.Domain/Entities/Course.cs
@@ -1,19 +1,31 @@
 ﻿using SMS.Domain.Common;
 
 namespace SMS.Domain.Entities;
-public class Course : BaseAuditableEntity
+public sealed class Course : BaseAuditableEntity
 {
-    public Course()
+    public Course(Guid id): base(id)
     {
         
     }
 
-    public Course(string courseName, string courseCode)
+    private Course(Guid id, string courseName, string courseCode, int unit) : base(id)
     {
-        CourseName = courseName;
-        CourseCode = courseCode;
+        Name = courseName;
+        Code = courseCode;
+        Unit = unit;
     }
 
-    public string CourseName { get; private set; }
-    public string CourseCode { get; private set; }
+    public string Name { get; private set; }
+    public string Code { get; private set; }
+    public int Unit { get; private set; }
+
+    public static Course Create(Guid id, string courseName, string courseCode, int unit)
+    {
+        Course course = new(id, courseName, courseCode, unit);
+
+        if (course.Unit > 4)
+            throw new Exception("Course unit can not be more than 4");
+
+        return course;
+    }
 }
