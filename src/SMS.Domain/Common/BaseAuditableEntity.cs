565cf63de46da26f43e02b6f3420a1959a78dfb8
Wale <onafesowale@gmail.com>
2023-12-03 06:51:49 +0100

    Entity

--- src/SMS.Domain/Common/BaseAuditableEntity.cs
+++ src/SMS.Domain/Common/BaseAuditableEntity.cs
@@ -1,6 +1,12 @@
-﻿namespace SMS.Domain.Common;
-public class BaseAuditableEntity : BaseEntity
+﻿using SMS.Domain.Primitives;
+
+namespace SMS.Domain.Common;
+public class BaseAuditableEntity : Entity
 {
+    public BaseAuditableEntity(Guid id) : base(id)
+    {
+        
+    }
     public DateTimeOffset CreatedAt { get;set; }
     public string CreatedBy { get; set; }
     public DateTimeOffset LastModified { get; set; }
