99d50efa712b5531039e3ee5ed73c318f0fa0ff5
Wale <onafesowale@gmail.com>
2023-12-03 06:51:03 +0100

    create course command

---
+++ src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using SMS.Domain.Entities;
+
+namespace SMS.Application.Courses.Commands.CreateCourse;
+internal record CreateCourseCommand(string Name, string Code, int Unit) : IRequest<int>;
+
+internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, int>
+{
+    public Task<int> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+    {
+        var course = Course.Create(
+            Guid.NewGuid(),
+            request.Name,
+            request.Code,
+            request.Unit);
+
+        throw new NotImplementedException();
+    }
+}
