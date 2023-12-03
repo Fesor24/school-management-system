0249fa23ea4e840ab9888fe1c3a1413e2d3069e5
Wale <onafesowale@gmail.com>
2023-12-03 06:54:35 +0100

    Course entity configuration

---
+++ src/SMS.Infrastructure/Data/Configurations/CourseEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SMS.Domain.Entities;
+
+namespace SMS.Infrastructure.Data.Configurations;
+public class CourseEntityConfiguration : IEntityTypeConfiguration<Course>
+{
+    public void Configure(EntityTypeBuilder<Course> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.ToTable(nameof(Course), "sch");
+
+        builder.Property(x => x.Code)
+            .HasMaxLength(5)
+            .HasColumnName("Code");
+
+        builder.Property(x => x.Name)
+            .HasMaxLength(150)
+            .HasColumnName("Name");
+    }
+}
