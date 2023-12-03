21c355fa01d789dfabab77c30e71429b577b7450
Wale <onafesowale@gmail.com>
2023-12-03 06:54:14 +0100

    Result generic class

---
+++ src/SMS.Domain/Shared/ResultT.cs
@@ -0,0 +1,15 @@
+﻿namespace SMS.Domain.Shared;
+public class Result<TValue> : Result
+{
+    private readonly TValue? _value;
+
+    protected internal Result(TValue? value, bool isSuccess, Error error)
+        : base(isSuccess, error) => 
+        _value = value;
+
+    public TValue Value => IsSuccess ?
+        _value :
+        throw new Exception("Value of result can not be accessed");
+
+    public static implicit operator Result<TValue>(TValue? value) => Create(value);
+}
