d5e6d74a178cf406df7274a839075310a3234e44
Wale <onafesowale@gmail.com>
2023-12-04 11:11:57 +0100

    IGenericRepository

---
+++ src/SMS.Domain/Abstractions/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using SMS.Domain.Primitives;
+
+namespace SMS.Domain.Abstractions;
+public interface IGenericRepository<TEntity> where TEntity: Entity, new()
+{
+    Task<List<TEntity>> GetAll();
+
+    Task<TEntity> GetAsync(Guid id);
+
+    Task AddAsync(TEntity entity);
+
+    void Update(TEntity entity);
+
+    void Delete(TEntity entity);
+}
