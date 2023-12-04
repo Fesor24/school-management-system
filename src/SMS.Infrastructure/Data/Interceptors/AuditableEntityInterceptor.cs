b14378111eefe29c64cbb6bc081251f46fd35572
Wale <onafesowale@gmail.com>
2023-12-04 11:20:11 +0100

    AuditableEntityInterceptor

---
+++ src/SMS.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using SMS.Domain.Common;
+
+namespace SMS.Infrastructure.Data.Interceptors;
+internal sealed class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, 
+        InterceptionResult<int> result)
+    {
+        UpdateEntities(eventData.Context);
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, 
+        InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        UpdateEntities(eventData.Context);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    public static void UpdateEntities(DbContext? context)
+    {
+        if (context is null) return;
+
+        var entities = context.ChangeTracker.Entries<BaseAuditableEntity>().ToList();
+
+        foreach(var entry in entities)
+        {
+            entry.Entity.LastModified = DateTimeOffset.Now;
+
+            if(entry.State == EntityState.Added)
+            {
+                entry.Entity.CreatedAt = DateTimeOffset.Now;
+            }
+        }
+    }
+}
