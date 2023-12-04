c6dbefab437bff80a43169ca432a73577fec7fb5
Wale <onafesowale@gmail.com>
2023-12-04 11:19:08 +0100

    Aggregate root

---
+++ src/SMS.Domain/Primitives/AggregateRoot.cs
@@ -0,0 +1,22 @@
+﻿using SMS.Domain.Common;
+
+namespace SMS.Domain.Primitives;
+public abstract class AggregateRoot : BaseAuditableEntity
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+
+    protected AggregateRoot() { }
+
+    protected AggregateRoot(Guid id) : base(id)
+    {
+        
+    }
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent) => 
+        _domainEvents.Add(domainEvent);
+
+    public void ClearDomainEvents() =>
+        _domainEvents.Clear();
+}
