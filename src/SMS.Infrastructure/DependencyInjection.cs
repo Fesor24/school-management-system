8589dcacf51c9a734850f0690fec96b7a2d2fc6f
Wale <onafesowale@gmail.com>
2023-12-04 11:21:09 +0100

    DependencyInjection

--- src/SMS.Infrastructure/DependencyInjection.cs
+++ src/SMS.Infrastructure/DependencyInjection.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SMS.Infrastructure.Data;
+using SMS.Infrastructure.Data.Interceptors;
 
 namespace SMS.Infrastructure;
 public static class DependencyInjection
@@ -9,9 +10,17 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, 
         IConfiguration configuration)
     {
-        services.AddDbContext<ApplicationDbContext>(opt =>
+        services.AddSingleton<AuditableEntityInterceptor>();
+
+        services.AddSingleton<PublishDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, opt) =>
         {
-            opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            opt.UseNpgsql(configuration.GetConnectionString("DefaultConnection"))
+            .AddInterceptors(
+                sp.GetRequiredService<AuditableEntityInterceptor>(),
+                sp.GetRequiredService<PublishDomainEventsInterceptor>()
+                );
         });
 
         return services;
