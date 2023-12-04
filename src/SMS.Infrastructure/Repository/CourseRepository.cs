cad3ec3fa1263731b0a73288e25e281a374c1fdf
Wale <onafesowale@gmail.com>
2023-12-04 11:21:23 +0100

    CourseRepository

---
+++ src/SMS.Infrastructure/Repository/CourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using SMS.Domain.Abstractions;
+using SMS.Domain.Entities;
+using SMS.Infrastructure.Data;
+
+namespace SMS.Infrastructure.Repository;
+internal class CourseRepository : GenericRepository<Course>, ICourseRepository
+{
+    public CourseRepository(ApplicationDbContext context) : base(context)
+    {
+        
+    }
+}
