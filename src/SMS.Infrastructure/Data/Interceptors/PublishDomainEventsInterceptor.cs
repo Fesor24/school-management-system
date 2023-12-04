d3b3dd1266e7add4869c8497e0a955f256ff5129
Wale <onafesowale@gmail.com>
2023-12-04 11:20:57 +0100

    PublishDomainEventsInterceptor

---
+++ src/SMS.Infrastructure/Data/Interceptors/PublishDomainEventsInterceptor.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using SMS.Domain.Common;
+using SMS.Domain.Primitives;
+
+namespace SMS.Infrastructure.Data.Interceptors;
+internal sealed class PublishDomainEventsInterceptor : SaveChangesInterceptor
+{
+    private readonly IPublisher _publisher;
+
+    public PublishDomainEventsInterceptor(IPublisher publisher)
+    {
+        _publisher = publisher;
+    }
+
+    public override int SavedChanges(SaveChangesCompletedEventData eventData, int result)
+    {
+        PublishDomainEvents(eventData.Context).GetAwaiter().GetResult();
+
+        return base.SavedChanges(eventData, result);
+    }
+
+    public override async ValueTask<int> SavedChangesAsync(SaveChangesCompletedEventData eventData, 
+        int result, CancellationToken cancellationToken = default)
+    {
+        await PublishDomainEvents(eventData.Context);
+
+        return result;
+    }
+
+    public async Task PublishDomainEvents(DbContext? context)
+    {
+        if (context is null) return;
+
+        var entities = context.ChangeTracker
+            .Entries<AggregateRoot>()
+            .Where(x => x.Entity.DomainEvents.Any())
+            .Select(x => x.Entity);
+
+        var domainEvents = entities
+            .SelectMany(x => x.DomainEvents)
+            .ToList();
+
+        entities.ToList().ForEach(x => x.ClearDomainEvents());
+
+        foreach(var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
+    }
+}
