0666d8b7ce42794553196b5a9199a5d2f8cbfe1b
Wale <onafesowale@gmail.com>
2023-12-05 09:13:36 +0100

    remove unused namespace

--- src/SMS.Domain/DomainEvents/CourseCreatedEvent.cs
+++ src/SMS.Domain/DomainEvents/CourseCreatedEvent.cs
@@ -1,9 +1,4 @@
 ﻿using SMS.Domain.Primitives;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SMS.Domain.DomainEvents;
 public sealed record CourseCreatedEvent(string CourseName, string CourseCode, int Unit) : IDomainEvent;
