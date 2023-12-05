d0692191f0eb6beca4fe0ef7e16b0fdc935f49a6
Wale <onafesowale@gmail.com>
2023-12-05 09:14:31 +0100

    removed error namespace

--- src/SMS.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
+++ src/SMS.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
-using SMS.Domain.Common;
+using SMS.Domain.Primitives;
 
 namespace SMS.Infrastructure.Data.Interceptors;
 internal sealed class AuditableEntityInterceptor : SaveChangesInterceptor
