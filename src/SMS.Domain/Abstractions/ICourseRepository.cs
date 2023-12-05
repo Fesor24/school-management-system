0000a1e7f9976ea9e1c926ef2a9ba0bb993d164b
Wale <onafesowale@gmail.com>
2023-12-05 09:13:25 +0100

    remove unused namespace

--- src/SMS.Domain/Abstractions/ICourseRepository.cs
+++ src/SMS.Domain/Abstractions/ICourseRepository.cs
@@ -1,5 +1,4 @@
-﻿using SMS.Domain.Common;
-using SMS.Domain.Entities;
+﻿using SMS.Domain.Entities;
 
 namespace SMS.Domain.Abstractions;
 public interface ICourseRepository : IGenericRepository<Course>
