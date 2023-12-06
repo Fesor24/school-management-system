3253c84ac4a979a3eafb968961ae239da4e56f15
Wale <onafesowale@gmail.com>
2023-12-06 11:20:13 +0100

    Failure result

--- src/SMS.Domain/Shared/Result.cs
+++ src/SMS.Domain/Shared/Result.cs
@@ -21,5 +21,6 @@ public class Result
     public static Result Failure(Error error) => new(false, error);
 
     public static Result<TValue> Success<TValue>(TValue value) => new(value, true, Error.None);
+    public static Result<TValue> Failure<TValue>(Error error) => new(false, error);
     public static Result<TValue> Create<TValue>(TValue value) => new(value, true, Error.None);
 }
