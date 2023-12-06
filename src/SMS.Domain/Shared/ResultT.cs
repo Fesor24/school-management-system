dddb6866e65abfc1e2e6cee16b5a12b96872cbc3
Wale <onafesowale@gmail.com>
2023-12-06 11:19:49 +0100

    generic result

--- src/SMS.Domain/Shared/ResultT.cs
+++ src/SMS.Domain/Shared/ResultT.cs
@@ -3,6 +3,8 @@ public class Result<TValue> : Result
 {
     private readonly TValue? _value;
 
+    protected internal Result(bool isSuccess, Error error) : base(isSuccess, error) { }
+
     protected internal Result(TValue? value, bool isSuccess, Error error)
         : base(isSuccess, error) => 
         _value = value;
@@ -11,5 +13,5 @@ public class Result<TValue> : Result
         _value :
         throw new Exception("Value of result can not be accessed");
 
-    public static implicit operator Result<TValue>(TValue? value) => Create(value);
+    public static implicit operator Result<TValue>(TValue value) => Create(value);
 }
