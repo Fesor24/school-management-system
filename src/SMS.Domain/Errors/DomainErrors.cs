0b41550e051411898bb6f555071eacbc86e33fc7
Wale <onafesowale@gmail.com>
2023-12-06 11:34:42 +0100

    Domain errors

---
+++ src/SMS.Domain/Errors/DomainErrors.cs
@@ -0,0 +1,12 @@
+﻿using SMS.Domain.Shared;
+
+namespace SMS.Domain.Errors;
+public static class DomainErrors
+{
+    public static class Course
+    {
+        public static readonly Error InvalidCourseUnit = new (
+            "Course.Unit", 
+            "Course unit must be greater than 0 and less than 7");
+    }
+}
