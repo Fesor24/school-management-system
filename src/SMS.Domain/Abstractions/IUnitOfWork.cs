1c3ae441940c7aaa86f2be794047e0d3c6251506
Wale <onafesowale@gmail.com>
2023-12-06 11:19:07 +0100

    Added course repository

--- src/SMS.Domain/Abstractions/IUnitOfWork.cs
+++ src/SMS.Domain/Abstractions/IUnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿namespace SMS.Domain.Abstractions;
 public interface IUnitOfWork : IDisposable
 {
+    public ICourseRepository CourseRepository { get; init; }
     Task<int> Complete(CancellationToken cancelleationToken = default);
 }
