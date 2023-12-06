c363eab7a2b6210cfa63b4a23a0de4ce7aeffeec
Wale <onafesowale@gmail.com>
2023-12-06 11:18:13 +0100

    implement handler

--- src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
+++ src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
@@ -1,19 +1,34 @@
 ﻿using MediatR;
+using SMS.Domain.Abstractions;
 using SMS.Domain.Entities;
+using SMS.Domain.Shared;
 
 namespace SMS.Application.Courses.Commands.CreateCourse;
-internal record CreateCourseCommand(string Name, string Code, int Unit) : IRequest<int>;
+internal record CreateCourseCommand(string Name, string Code, int Unit) : IRequest<Guid>;
 
-internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, int>
+internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, Guid>
 {
-    public Task<int> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+    private readonly IUnitOfWork _unitOfWork;
+
+    internal CreateCourseCommandHandler(IUnitOfWork unitOfWork)
     {
-        var course = Course.Create(
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Guid> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+    {
+        var result = Course.Create(
             Guid.NewGuid(),
             request.Name,
             request.Code,
             request.Unit);
 
-        throw new NotImplementedException();
+        //if (result.IsFailure) return result.Error;
+
+        var course = result.Value;
+
+        await _unitOfWork.CourseRepository.AddAsync(course);
+
+        return await _unitOfWork.Complete() > 0 ? course.Id : default;
     }
 }
