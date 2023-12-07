fcd99d243a1173ca8e9de8aba222440457ae6e64
Wale <onafesowale@gmail.com>
2023-12-07 08:57:48 +0100

    publish create course event

--- src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
+++ src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
@@ -1,17 +1,23 @@
 ﻿using MediatR;
+using Microsoft.Extensions.Logging;
 using SMS.Domain.Abstractions;
+using SMS.Domain.DomainEvents;
 using SMS.Domain.Entities;
 
 namespace SMS.Application.Courses.Commands.CreateCourse;
-internal record CreateCourseCommand(string Name, string Code, int Unit) : IRequest<Guid>;
+public record CreateCourseCommand(string Name, string Code, int Unit) : IRequest<Guid>;
 
 internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, Guid>
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IPublisher _publisher;
+    private readonly ILogger<CreateCourseCommandHandler> _logger;
 
-    internal CreateCourseCommandHandler(IUnitOfWork unitOfWork)
+    internal CreateCourseCommandHandler(IUnitOfWork unitOfWork, IPublisher publisher, ILogger<CreateCourseCommandHandler> logger)
     {
         _unitOfWork = unitOfWork;
+        _publisher = publisher;
+        _logger = logger;
     }
 
     public async Task<Guid> Handle(CreateCourseCommand request, 
@@ -23,12 +29,22 @@ internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseC
             request.Code,
             request.Unit);
 
-        //if (result.IsFailure) return result.Error;
+        if (result.IsFailure)
+        {
+            _logger.LogError(result.Error);
+
+            return default;
+        }
 
         var course = result.Value;
 
         await _unitOfWork.CourseRepository.AddAsync(course);
 
-        return await _unitOfWork.Complete() > 0 ? course.Id : default;
+        var res = await _unitOfWork.Complete(cancellationToken) > 0;
+
+        if (res)
+            await _publisher.Publish(new CourseCreatedEvent(course.Name, course.Code, course.Unit));
+
+        return res ? course.Id : default;
     }
 }
