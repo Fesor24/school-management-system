74782ccb30f4f87f6b089874da2151af53f648cd
Wale <onafesowale@gmail.com>
2023-12-07 19:54:14 +0100

    not found exception

---
+++ src/SMS.Domain/Exceptions/BaseExceptions/NotFoundException.cs
@@ -0,0 +1,8 @@
+﻿namespace SMS.Domain.Exceptions.BaseExceptions;
+public class NotFoundException : Exception
+{
+    public NotFoundException(string message)  : base(message)
+    {
+        
+    }
+}
