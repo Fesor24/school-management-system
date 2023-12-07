2bdb083a2a0ca13d9c6fcdcffda61eb018913d6b
Wale <onafesowale@gmail.com>
2023-12-07 19:55:05 +0100

    course not found exception

---
+++ src/SMS.Domain/Exceptions/Course/CourseNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using SMS.Domain.Exceptions.BaseExceptions;
+
+namespace SMS.Domain.Exceptions.Course;
+public sealed class CourseNotFoundException : NotFoundException
+{
+    public CourseNotFoundException(Guid id) : base($"Course with Id: {id} not found")
+    {
+        
+    }
+}
