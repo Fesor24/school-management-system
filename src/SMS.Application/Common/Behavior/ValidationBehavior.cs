b12b714d306854dc60ee3672c1c1e740d43c013e
Wale <onafesowale@gmail.com>
2023-12-07 19:50:57 +0100

    validation behavior

---
+++ src/SMS.Application/Common/Behavior/ValidationBehavior.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace SMS.Application.Common.Behavior;
+internal class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+{
+    public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
