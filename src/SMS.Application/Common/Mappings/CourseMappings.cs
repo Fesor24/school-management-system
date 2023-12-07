fd16dfb067558136727a53a394208db995ecc672
Wale <onafesowale@gmail.com>
2023-12-07 19:51:07 +0100

    mappings

---
+++ src/SMS.Application/Common/Mappings/CourseMappings.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using SMS.Application.Courses.Response;
+using SMS.Domain.Entities;
+
+namespace SMS.Application.Common.Mappings;
+internal class CourseMappings : Profile
+{
+    public CourseMappings()
+    {
+        CreateMap<Course, GetCourseResponse>();
+    }
+}
