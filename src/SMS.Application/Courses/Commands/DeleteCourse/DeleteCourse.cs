be48b8e6d4ef7eb1ef19125813c036c2def579f4
Wale <onafesowale@gmail.com>
2023-12-07 19:51:57 +0100

    delete course functionality

---
+++ src/SMS.Application/Courses/Commands/DeleteCourse/DeleteCourse.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using SMS.Domain.Abstractions;
+using SMS.Domain.Exceptions.Course;
+
+namespace SMS.Application.Courses.Commands.DeleteCourse;
+internal record DeleteCourseCommand(Guid Id) : IRequest<Unit>;
+
+internal sealed class DeleteCourseCommadHandler : IRequestHandler<DeleteCourseCommand, Unit>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteCourseCommadHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Unit> Handle(DeleteCourseCommand request, CancellationToken cancellationToken)
+    {
+        var course = await _unitOfWork.CourseRepository.GetAsync(request.Id) ??
+            throw new CourseNotFoundException(request.Id);
+
+        _unitOfWork.CourseRepository.Delete(course);
+
+        await _unitOfWork.Complete();
+
+        return Unit.Value;
+    }
+}
