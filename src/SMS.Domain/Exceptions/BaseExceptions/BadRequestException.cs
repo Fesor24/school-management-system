21a78506dff6bd9687d12bfd9756c09cbf9b4ba6
Wale <onafesowale@gmail.com>
2023-12-07 19:54:04 +0100

    bad request exception

---
+++ src/SMS.Domain/Exceptions/BaseExceptions/BadRequestException.cs
@@ -0,0 +1,8 @@
+﻿namespace SMS.Domain.Exceptions.BaseExceptions;
+public class BadRequestException : Exception
+{
+    public BadRequestException(string message) : base(message)
+    {
+        
+    }
+}
