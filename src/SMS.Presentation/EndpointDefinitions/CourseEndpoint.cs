f72190ae46023517798457f410ceafa77e739d7e
Wale <onafesowale@gmail.com>
2023-12-07 08:59:05 +0100

    Course endpoint

---
+++ src/SMS.Presentation/EndpointDefinitions/CourseEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using SMS.Application.Courses.Commands.CreateCourse;
+using SMS.Presentation.Abstractions;
+using SMS.Presentation.Extensions;
+
+namespace SMS.Presentation.EndpointDefinitions;
+internal class CourseEndpoint : IEndpointDefinition
+{
+    public void RegisterEndpoints(WebApplication app)
+    {
+        const string ENDPOINT = "course";
+
+        app.MediatorPost<CreateCourseCommand, Guid>(ENDPOINT, "/");
+    }
+}
