4b4cd8b4662cac84d350acd91950af968a054e9a
Wale <onafesowale@gmail.com>
2023-12-07 08:58:14 +0100

    create course event handler

---
+++ src/SMS.Application/Courses/Events/CourseCreatedEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SMS.Domain.DomainEvents;
+
+namespace SMS.Application.Courses.Events;
+internal class CourseCreatedEventHandler : INotificationHandler<CourseCreatedEvent>
+{
+    private readonly ILogger<CourseCreatedEventHandler> _logger;
+
+    public CourseCreatedEventHandler(ILogger<CourseCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(CourseCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        Console.WriteLine($"A course was created. Name: {notification.CourseName}, Unit: {notification.Unit}, " +
+            $"Code: {notification.CourseCode}");
+
+        _logger.LogInformation($"A course was created. Name: {notification.CourseName}, Unit: {notification.Unit}, " +
+            $"Code: {notification.CourseCode}");
+
+        return Task.CompletedTask;
+    }
+}
