529f5c475fb29ec431a3a387c22505d50311b4ce
Wale <onafesowale@gmail.com>
2023-12-07 19:52:17 +0100

    get course by id

---
+++ src/SMS.Application/Courses/Queries/GetCourseById/GetCourseById.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using MediatR;
+using SMS.Application.Courses.Response;
+using SMS.Domain.Abstractions;
+using SMS.Domain.Exceptions.Course;
+
+namespace SMS.Application.Courses.Queries.GetCourseById;
+internal record GetCourseByIdRequest(Guid Id) : IRequest<GetCourseResponse>;
+
+internal sealed class GetCourseByIdRequestHandler : IRequestHandler<GetCourseByIdRequest, GetCourseResponse>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetCourseByIdRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<GetCourseResponse> Handle(GetCourseByIdRequest request, 
+        CancellationToken cancellationToken)
+    {
+        var course = await _unitOfWork.CourseRepository.GetAsync(request.Id, cancellationToken) ??
+            throw new CourseNotFoundException(request.Id);
+
+        return _mapper.Map<GetCourseResponse>(course);
+    }
+}
