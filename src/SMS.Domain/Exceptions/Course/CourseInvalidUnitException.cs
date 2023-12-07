d9900b8e2094d3cafad9f8f837db5b13d027b802
Wale <onafesowale@gmail.com>
2023-12-07 19:54:56 +0100

    Course invalid unit exception

---
+++ src/SMS.Domain/Exceptions/Course/CourseInvalidUnitException.cs
@@ -0,0 +1,10 @@
+﻿using SMS.Domain.Exceptions.BaseExceptions;
+
+namespace SMS.Domain.Exceptions.Course;
+public class CourseInvalidUnitException : BadRequestException
+{
+    public CourseInvalidUnitException(string message) : base(message)
+    {
+        
+    }
+}
