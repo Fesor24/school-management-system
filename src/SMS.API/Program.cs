faf5ccc7a3e9590eaa73f8d49d1ed3e2ad6af6d9
Wale <onafesowale@gmail.com>
2023-12-07 08:57:08 +0100

    serilog config

--- src/SMS.API/Program.cs
+++ src/SMS.API/Program.cs
@@ -1,14 +1,21 @@
+using Serilog;
 using SMS.Application;
 using SMS.Infrastructure;
+using SMS.Presentation.Extensions;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddInfrastructureServices(builder.Configuration)
     .AddApplicationServices();
 
-
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSerilogRequestLogging();
+
+app.RegisterEndpoints();
 
 app.Run();
