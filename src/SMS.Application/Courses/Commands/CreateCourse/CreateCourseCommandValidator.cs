cf5d4f36ee0187b4a211226118347baf9577180a
Wale <onafesowale@gmail.com>
2023-12-07 08:58:02 +0100

    Create course command validator

--- src/SMS.Application/Courses/Commands/CreateCourse/CreateCourseCommandValidator.cs
+++ src/SMS.Application/Courses/Commands/CreateCourse/CreateCourseCommandValidator.cs
@@ -1,4 +1,16 @@
-﻿namespace SMS.Application.Courses.Commands.CreateCourse;
-internal class CreateCourseCommandValidator
+﻿using FluentValidation;
+
+namespace SMS.Application.Courses.Commands.CreateCourse;
+internal class CreateCourseCommandValidator : AbstractValidator<CreateCourseCommand>
 {
+    public CreateCourseCommandValidator()
+    {
+        RuleFor(x => x.Code)
+            .NotEmpty().WithMessage("Course code can not be empty")
+            .NotNull().WithMessage("Course code can not be null");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Course name can not be empty")
+            .NotNull().WithMessage("Course name can not be null");
+    }
 }
