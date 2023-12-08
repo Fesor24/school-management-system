9fd03854f0500c3c6a3a525172f9be395fbd4e0a
Wale <onafesowale@gmail.com>
2023-12-08 08:43:32 +0100

    IUnitOfWork

---
+++ src/SMS.Domain/Primitives/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using SMS.Domain.Aggregates.DepartmentAggregates;
+
+namespace SMS.Domain.Primitives;
+public interface IUnitOfWork : IDisposable
+{
+    public ICourseRepository CourseRepository { get; init; }
+    Task<int> Complete(CancellationToken cancelleationToken = default);
+}
