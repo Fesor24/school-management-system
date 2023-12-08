387ca96076a6120d5e005e3e7a108bd7941dc24c
Wale <onafesowale@gmail.com>
2023-12-08 08:40:32 +0100

    removed file

--- src/SMS.Domain/Abstractions/IUnitOfWork.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SMS.Domain.Abstractions;
-public interface IUnitOfWork : IDisposable
-{
-    public ICourseRepository CourseRepository { get; init; }
-    Task<int> Complete(CancellationToken cancelleationToken = default);
-}
