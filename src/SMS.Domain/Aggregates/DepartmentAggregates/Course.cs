2f69b9d00faaccaa6091b408d015895fc73338eb
Wale <onafesowale@gmail.com>
2023-12-08 08:41:00 +0100

    course entity

---
+++ src/SMS.Domain/Aggregates/DepartmentAggregates/Course.cs
@@ -0,0 +1,38 @@
+﻿using SMS.Domain.DomainEvents.Course;
+using SMS.Domain.Errors;
+using SMS.Domain.Primitives;
+using SMS.Domain.Shared;
+
+namespace SMS.Domain.Aggregates.DepartmentAggregates;
+public sealed class Course : Entity
+{
+    public Course()
+    {
+
+    }
+
+    private Course(Guid id, string courseName, string courseCode, int unit) : base(id)
+    {
+        CourseInfo = new(courseName, courseCode);
+        Unit = unit;
+    }
+
+    public CourseInfo CourseInfo { get; private set; }
+
+    public int Unit { get; private set; }
+
+    public static Result<Course> Create(Guid id, string courseName, string courseCode, int unit)
+    {
+        Course course = new(id, courseName, courseCode, unit);
+
+        if (course.Unit > 6)
+            return Result.Failure<Course>(DomainErrors.Course.InvalidCourseUnit);
+
+        course.AddDomainEvent(new CourseCreatedEvent(
+            course.CourseInfo.Name, 
+            course.CourseInfo.Code, 
+            course.Unit));
+
+        return course;
+    }
+}
