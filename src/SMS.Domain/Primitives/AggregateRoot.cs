3b672c78e20e015db64a4c8354730828a2f55127
Wale <onafesowale@gmail.com>
2023-12-08 08:42:35 +0100

    removed domain event from aggregate

--- src/SMS.Domain/Primitives/AggregateRoot.cs
+++ src/SMS.Domain/Primitives/AggregateRoot.cs
@@ -1,23 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace SMS.Domain.Primitives;
+﻿namespace SMS.Domain.Primitives;
 public abstract class AggregateRoot : BaseAuditableEntity
 {
-    private readonly List<IDomainEvent> _domainEvents = new();
-
     protected AggregateRoot() { }
 
     protected AggregateRoot(Guid id) : base(id)
     {
         
     }
-
-    [NotMapped]
-    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
-
-    protected void AddDomainEvent(IDomainEvent domainEvent) => 
-        _domainEvents.Add(domainEvent);
-
-    public void ClearDomainEvents() =>
-        _domainEvents.Clear();
 }
