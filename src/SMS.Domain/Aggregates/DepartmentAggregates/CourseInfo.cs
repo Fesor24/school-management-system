7fe520bd4b5b0c3d3584b5355eb602f1f74027f1
Wale <onafesowale@gmail.com>
2023-12-08 08:41:17 +0100

    course info value object

---
+++ src/SMS.Domain/Aggregates/DepartmentAggregates/CourseInfo.cs
@@ -0,0 +1,20 @@
+﻿using SMS.Domain.Primitives;
+
+namespace SMS.Domain.Aggregates.DepartmentAggregates;
+public class CourseInfo : ValueObject
+{
+    public CourseInfo(string name, string code)
+    {
+        Name = name;
+        Code = code;
+    }
+
+    public string Name { get; private set; }
+    public string Code { get; private set; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Name;
+        yield return Code;
+    }
+}
