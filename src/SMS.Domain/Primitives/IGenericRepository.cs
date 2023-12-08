d010f651bd23d853a422bbda82e40a3512e28625
Wale <onafesowale@gmail.com>
2023-12-08 08:43:22 +0100

    IGenericRepository

---
+++ src/SMS.Domain/Primitives/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace SMS.Domain.Primitives;
+public interface IGenericRepository<TEntity> where TEntity : Entity, new()
+{
+    Task<List<TEntity>> GetAll(CancellationToken cancellationToken = default);
+
+    Task<TEntity?> GetAsync(Guid id, CancellationToken cancellationToken = default);
+
+    Task AddAsync(TEntity entity);
+
+    void Update(TEntity entity);
+
+    void Delete(TEntity entity);
+}
