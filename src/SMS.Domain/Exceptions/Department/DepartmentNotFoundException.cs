9153d5c2d15cbe865e25b8b9d079c3b3f7b0f71b
Wale <onafesowale@gmail.com>
2023-12-09 08:36:47 +0100

    department not found exception

---
+++ src/SMS.Domain/Exceptions/Department/DepartmentNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using SMS.Domain.Exceptions.BaseExceptions;
+
+namespace SMS.Domain.Exceptions.Department;
+public class DepartmentNotFoundException : NotFoundException
+{
+    public DepartmentNotFoundException(Guid id) : base($"Department with Id: {id} not found")
+    {
+        
+    }
+}
