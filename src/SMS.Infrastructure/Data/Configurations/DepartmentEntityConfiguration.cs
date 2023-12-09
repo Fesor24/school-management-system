3ad5870e98c88833d93f8034fededc258f2179b7
Wale <onafesowale@gmail.com>
2023-12-09 08:37:24 +0100

    entity configuration

---
+++ src/SMS.Infrastructure/Data/Configurations/DepartmentEntityConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SMS.Domain.Aggregates.DepartmentAggregates;
+
+namespace SMS.Infrastructure.Data.Configurations;
+public class DepartmentEntityConfiguration : IEntityTypeConfiguration<Department>
+{
+    public void Configure(EntityTypeBuilder<Department> builder)
+    {
+        builder.ToTable(nameof(Department), "sms");
+
+        builder.HasKey(x => x.Id);
+
+        builder.HasMany(x => x.Courses)
+            .WithOne();
+
+        builder.Property<List<Course>>("_courses")
+            .UsePropertyAccessMode(PropertyAccessMode.Field)
+            .HasColumnName("Courses");
+
+        builder.Ignore(x => x.DomainEvents);
+    }
+}
