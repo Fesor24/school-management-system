ce886bdbf9a480aef02dd37e3af97ff40f3cee98
Wale <onafesowale@gmail.com>
2023-12-09 08:37:33 +0100

    department repository

---
+++ src/SMS.Infrastructure/Repository/DepartmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Domain.Aggregates.DepartmentAggregates;
+using SMS.Infrastructure.Data;
+
+namespace SMS.Infrastructure.Repository;
+internal class DepartmentRepository : GenericRepository<Department>, IDepartmentRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public DepartmentRepository(ApplicationDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<Course?> GetCourseAsync(Guid courseId, CancellationToken cancellationToken = default) => 
+        await _context.Courses
+        .FirstOrDefaultAsync(x => x.Id == courseId, cancellationToken);
+
+    public async Task<Department?> GetDepartmentInfo(Guid departmentId, CancellationToken cancellationtoken = default) => 
+        await _context.Departments
+        .Include(x => x.Courses)
+        .FirstOrDefaultAsync(x => x.Id == departmentId, cancellationtoken);
+}
