c0a383ccfd558d2bd15b0097ef0136d74772baf5
Wale <onafesowale@gmail.com>
2023-12-09 08:33:44 +0100

    course deleted event

---
+++ src/SMS.Application/Courses/Events/CourseDeletedEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SMS.Domain.DomainEvents.Course;
+
+namespace SMS.Application.Courses.Events;
+internal sealed class CourseDeletedEventHandler : INotificationHandler<CourseDeletedEvent>
+{
+    private readonly ILogger<CourseDeletedEventHandler> _logger;
+
+    public CourseDeletedEventHandler(ILogger<CourseDeletedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(CourseDeletedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation($"Course with Id: {notification.CourseId} has been removed from database");
+
+        return Task.CompletedTask;
+    }
+}
