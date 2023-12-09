d333ee3f3d549be9ef8501e040889e8a7687b055
Wale <onafesowale@gmail.com>
2023-12-09 08:33:10 +0100

    modified handler

--- src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
+++ src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
@@ -1,10 +1,11 @@
 ﻿using MediatR;
 using SMS.Domain.Aggregates.DepartmentAggregates;
 using SMS.Domain.Exceptions.Course;
+using SMS.Domain.Exceptions.Department;
 using SMS.Domain.Primitives;
 
 namespace SMS.Application.Courses.Commands.CreateCourse;
-public record CreateCourseCommand(string Name, string Code, int Unit) : IRequest<Guid>;
+public record CreateCourseCommand(string Name, string Code, int Unit, Guid DepartmentId) : IRequest<Guid>;
 
 internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, Guid>
 {
@@ -18,18 +19,17 @@ internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseC
     public async Task<Guid> Handle(CreateCourseCommand request, 
         CancellationToken cancellationToken)
     {
-        var result = Course.Create(
-            Guid.NewGuid(),
-            request.Name,
-            request.Code,
-            request.Unit);
+        var department = await _unitOfWork.DepartmentRepository.GetDepartmentInfo(request.DepartmentId, cancellationToken) ??
+            throw new DepartmentNotFoundException(request.DepartmentId);
+
+        var result = department.AddCourse(request.Name, request.Code, request.Unit);
 
         if (result.IsFailure)
             throw new CourseInvalidUnitException(result.Error.Message);
 
-        var course = result.Value;
+        Course course = result.Value;
 
-        await _unitOfWork.CourseRepository.AddAsync(course);
+        _unitOfWork.DepartmentRepository.Update(department);
 
         return await _unitOfWork.Complete(cancellationToken) > 0 ? course.Id : default;
     }
