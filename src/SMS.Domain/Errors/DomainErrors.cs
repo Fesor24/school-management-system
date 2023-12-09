021bf58d772bbe8c8eb91ab2a94dc992f84e0f6d
Wale <onafesowale@gmail.com>
2023-12-09 08:36:34 +0100

    domain errors

--- src/SMS.Domain/Errors/DomainErrors.cs
+++ src/SMS.Domain/Errors/DomainErrors.cs
@@ -8,5 +8,9 @@ public static class DomainErrors
         public static readonly Error InvalidCourseUnit = new (
             "Course.Unit", 
             "Course unit must be greater than 0 and less than 7");
+
+        public static readonly Error CourseNotFound = new(
+            "Course.NotFound",
+            "Course not found");
     }
 }
