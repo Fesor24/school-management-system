3a31d0f5ad15246d63331432b0a8012b43efcb14
Wale <onafesowale@gmail.com>
2023-12-10 05:24:03 +0100

    mediatr extensions

---
+++ src/SMS.API/Extensions/MediatrExtensions.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+
+namespace SMS.API.Extensions;
+public static class MediatrExtensions
+{
+    public static void MediatorGet<TRequest, TResponse>(this WebApplication app, string endpointGroup, 
+        string route) where TRequest: IRequest<TResponse>
+    {
+        var group = app.MapGroup(endpointGroup);
+
+        group.MapGet(route, EndpointWithParametersAttribute<TRequest, TResponse>);
+    }
+
+    public static void MediatorPost<TRequest, TResponse>(this WebApplication app, string endpointGroup,
+        string route) where TRequest : IRequest<TResponse>
+    {
+        var group = app.MapGroup(endpointGroup);
+
+        group.MapPost(route, EndpointWithoutParametersAttribute<TRequest, TResponse>);
+    }
+
+    internal static async Task<IResult> EndpointWithParametersAttribute<TRequest, TResponse>([AsParameters] TRequest request, 
+        ISender sender, CancellationToken cancellationToken) where TRequest : IRequest<TResponse>
+    {
+        var response = await sender.Send(request, cancellationToken);
+
+        return Results.Ok(response);
+    }
+
+    internal static async Task<IResult> EndpointWithoutParametersAttribute<TRequest, TResponse>(TRequest request, 
+        IMediator mediator, CancellationToken cancellationToken) where TRequest : IRequest<TResponse>
+    {
+        var response = await mediator.Send(request, cancellationToken);
+
+        return Results.Ok(response);
+    }
+}
