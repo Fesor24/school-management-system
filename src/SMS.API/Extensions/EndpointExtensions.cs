7a5c135f142781488e1daee0f3e6b4ca64897ff0
Wale <onafesowale@gmail.com>
2023-12-10 05:24:18 +0100

    endpoint extensions

---
+++ src/SMS.API/Extensions/EndpointExtensions.cs
@@ -0,0 +1,18 @@
+﻿using SMS.API.Abstractions;
+using System.Reflection;
+
+namespace SMS.API.Extensions;
+public static class EndpointExtensions
+{
+    public static void RegisterEndpoints(this WebApplication app)
+    {
+        var endpointDefinitions = Assembly.GetExecutingAssembly()
+            .GetExportedTypes()
+            .Where(x => x.IsAssignableTo(typeof(IEndpointDefinition)) && !x.IsAbstract && !x.IsInterface)
+            .Select(Activator.CreateInstance)
+            .Cast<IEndpointDefinition>();
+
+        foreach (var endpoint in endpointDefinitions)
+            endpoint.RegisterEndpoints(app);
+    }
+}
