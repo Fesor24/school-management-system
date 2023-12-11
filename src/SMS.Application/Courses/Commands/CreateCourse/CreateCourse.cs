cc0ff08c2a2e47292c06ce378471b55a1fab8418
Wale <onafesowale@gmail.com>
2023-12-11 10:01:06 +0100

    modified response type

--- src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
+++ src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
@@ -1,13 +1,14 @@
 ﻿using MediatR;
 using SMS.Domain.Aggregates.DepartmentAggregates;
+using SMS.Domain.Errors;
 using SMS.Domain.Exceptions.Course;
 using SMS.Domain.Exceptions.Department;
 using SMS.Domain.Primitives;
 
 namespace SMS.Application.Courses.Commands.CreateCourse;
-public record CreateCourseCommand(string Name, string Code, int Unit, Guid DepartmentId) : IRequest<Guid>;
+public record CreateCourseCommand(string Name, string Code, int Unit, Guid DepartmentId) : IRequest<Unit>;
 
-internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, Guid>
+internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, Unit>
 {
     private readonly IUnitOfWork _unitOfWork;
 
@@ -16,7 +17,7 @@ internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseC
         _unitOfWork = unitOfWork;
     }
 
-    public async Task<Guid> Handle(CreateCourseCommand request, 
+    public async Task<Unit> Handle(CreateCourseCommand request, 
         CancellationToken cancellationToken)
     {
         var department = await _unitOfWork.DepartmentRepository.GetDepartmentInfo(request.DepartmentId, cancellationToken) ??
@@ -24,13 +25,13 @@ internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseC
 
         var result = department.AddCourse(request.Name, request.Code, request.Unit);
 
-        if (result.IsFailure)
+        if (result.IsFailure && result.Error.Equals(DomainErrors.Course.InvalidCourseUnit))
             throw new CourseInvalidUnitException(result.Error.Message);
 
         Course course = result.Value;
 
         _unitOfWork.DepartmentRepository.Update(department);
 
-        return await _unitOfWork.Complete(cancellationToken) > 0 ? course.Id : default;
+        return Unit.Value;
     }
 }
