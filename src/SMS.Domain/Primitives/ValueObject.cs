4a8b99b8b683757317a7190438a275cc14337dab
Wale <onafesowale@gmail.com>
2023-12-11 10:03:44 +0100

    operator overloading

--- src/SMS.Domain/Primitives/ValueObject.cs
+++ src/SMS.Domain/Primitives/ValueObject.cs
@@ -18,6 +18,16 @@ public abstract class ValueObject
 
     protected abstract IEnumerable<object> GetEqualityComponents();
 
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        return left is not null && right is not null && left.Equals(right);
+    }
+
+    public static bool operator !=(ValueObject? left, ValueObject? right)
+    {
+        return !(left == right);
+    }
+
     public override bool Equals(object obj)
     {
         if (obj == null || obj.GetType() != GetType())
