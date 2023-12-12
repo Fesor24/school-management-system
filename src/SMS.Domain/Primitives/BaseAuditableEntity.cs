1c492e0b65b26f257f5232cc308399e0c35ffc56
Wale <onafesowale@gmail.com>
2023-12-12 10:53:35 +0100

    BaseAuditableEntity

--- src/SMS.Domain/Primitives/BaseAuditableEntity.cs
+++ src/SMS.Domain/Primitives/BaseAuditableEntity.cs
@@ -8,7 +8,7 @@ public class BaseAuditableEntity : Entity
 
     }
     public DateTimeOffset CreatedAt { get; set; }
-    public string CreatedBy { get; set; }
+    public string? CreatedBy { get; set; }
     public DateTimeOffset LastModified { get; set; }
-    public string LastModifiedBy { get; set; }
+    public string? LastModifiedBy { get; set; }
 }
