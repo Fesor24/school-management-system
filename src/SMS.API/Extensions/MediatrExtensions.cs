4fc8836b549816df3eee65213d689bb8b063e0ee
Wale <onafesowale@gmail.com>
2023-12-12 18:13:47 +0100

    modified mediatr extensions

--- src/SMS.API/Extensions/MediatrExtensions.cs
+++ src/SMS.API/Extensions/MediatrExtensions.cs
@@ -1,84 +1,107 @@
 ﻿using MediatR;
-using SMS.API.Common.EndpointResponseMap;
+using SMS.API.Common.EndpointRouteMapper;
 using SMS.Application.Common.Response;
 using SMS.Domain.Shared;
 
 namespace SMS.API.Extensions;
 public static class MediatrExtensions
 {
-    public static void MediatorGet<TRequest, TResponse, TValue, TError>(this WebApplication app, string endpointGroup, 
+    public static void MediatorGet<TRequest, TResponse>(this WebApplication app, string endpointGroup, 
         string route, string routeName = "") 
-        where TRequest: IRequest<TResponse> 
-        where TResponse : Result<TValue, TError> 
-        where TError : Error
+        where TRequest: IRequest<Result<TResponse, Error>>
     {
         var group = app.MapGroup(endpointGroup)
             .WithName(routeName);
 
-        group.MapGet(route, EndpointWithParametersAttribute<TRequest, TResponse, TValue, TError>);
+        group.MapGet(route, EndpointForGetRequests<TRequest, TResponse>);
     }
 
-    public static void MediatorPost<TRequest, TResponse, TValue, TError>(this WebApplication app, string endpointGroup,
+    public static void MediatorPost<TRequest, TResponse>(this WebApplication app, string endpointGroup,
         string route) 
-        where TRequest : IRequest<TResponse>
-        where TResponse : Result<TValue, TError>
-        where TError : Error
-        where TValue : CreateResponse
+        where TRequest : IRequest<Result<TResponse, Error>>
+        where TResponse : CreateResponse
     {
         var group = app.MapGroup(endpointGroup);
 
-        group.MapPost(route, EndpointWithoutParametersAttribute<TRequest, TResponse, TValue, TError>);
+        group.MapPost(route, EndpointForCreateRequests<TRequest, TResponse>);
     }
 
     public static void MediatorPut<TRequest, TResponse>(this WebApplication app, string endpointGroup, 
-        string route) where TRequest : IRequest<TResponse>
+        string route) where TRequest : IRequest<Result<TResponse, Error>>
     {
         var group = app.MapGroup(endpointGroup);
 
-        group.MapPut(route, EndpointWithoutParametersAttribute<TRequest, TResponse>);
+        group.MapPut(route, EndpointForUpdateRequests<TRequest, TResponse>);
     }
 
-    internal static async Task<IResult> EndpointWithParametersAttribute<TRequest, TResponse, TValue, TError>
+    public static void MediatorDelete<TRequest, TResponse>(this WebApplication app, string endpointGroup,
+        string route) where TRequest : IRequest<Result<TResponse, Error>>
+    {
+        var group = app.MapGroup(endpointGroup);
+
+        group.MapDelete(route, EndpointForDeleteRequests<TRequest, TResponse>);
+    }
+
+    private static async Task<IResult> EndpointForGetRequests<TRequest, TResponse>
         ([AsParameters] TRequest request, 
         ISender sender, CancellationToken cancellationToken) 
-        where TRequest : IRequest<TResponse>
-        where TResponse : Result<TValue, TError>
-        where TError : Error
+        where TRequest : IRequest<Result<TResponse, Error>>
     {
         var response = await sender.Send(request, cancellationToken);
 
         return response.Match(
             suc => Results.Ok(suc),
-            failure => Results.NotFound(failure)
+            HandleError
             );
     }
 
-    internal static async Task<IResult> EndpointWithoutParametersAttribute<TRequest, TResponse, TValue, TError>
+    private static async Task<IResult> EndpointForCreateRequests<TRequest, TResponse>
         (TRequest request, 
         IMediator mediator, CancellationToken cancellationToken) 
-        where TRequest : IRequest<TResponse>
-        where TResponse : Result<TValue, TError>
-        where TError : Error
-        where TValue : CreateResponse
+        where TRequest : IRequest<Result<TResponse, Error>>
+        where TResponse : CreateResponse
     {
         var response = await mediator.Send(request, cancellationToken);
 
-        
-
         return response.Match(
-            suc => Results.CreatedAtRoute(ResponseRoute.GetRouteName(typeof(TValue)), 
+            suc => Results.CreatedAtRoute(EndpointRoutes.GetRouteName(typeof(TResponse)), 
             new { response.Value.Id }, suc),
-            failure => Results.BadRequest(failure)
+            HandleError
             );
     }
 
-    internal static async Task<IResult> EndpointWithoutParametersAttribute<TRequest, TResponse>
+    private static async Task<IResult> EndpointForUpdateRequests<TRequest, TResponse>
        (TRequest request,
-       IMediator mediator, CancellationToken cancellationToken)
-       where TRequest : IRequest<TResponse>
+       ISender sender, CancellationToken cancellationToken)
+       where TRequest : IRequest<Result<TResponse, Error>>
     {
-        var response = await mediator.Send(request, cancellationToken);
+        var response = await sender.Send(request, cancellationToken);
 
-        return Results.Ok(response);
+        return response.Match(
+            suc => Results.NoContent(),
+            HandleError
+            );
+    }
+
+    private static async Task<IResult> EndpointForDeleteRequests<TRequest, TResponse>(
+        TRequest request, ISender sender, CancellationToken cancellationToken
+        ) where TRequest : IRequest<Result<TResponse, Error>>
+    {
+        var response = await sender.Send(request, cancellationToken);
+
+        return response.Match(
+            suc => Results.NoContent(),
+            HandleError
+            );
+    }
+
+    private static IResult HandleError(Error error)
+    {
+        return error.Code switch
+        {
+            Domain.Errors.StatusCodes.NOTFOUND => Results.NotFound(error),
+            Domain.Errors.StatusCodes.BADREQUEST => Results.BadRequest(error),
+            _ => Results.BadRequest()
+        };
     }
 }
