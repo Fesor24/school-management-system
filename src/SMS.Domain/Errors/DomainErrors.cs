63a25eddf00816cd94f58954ea1c6727d00610ac
Wale <onafesowale@gmail.com>
2023-12-12 18:16:41 +0100

    Use domain statis codes

--- src/SMS.Domain/Errors/DomainErrors.cs
+++ src/SMS.Domain/Errors/DomainErrors.cs
@@ -6,16 +6,18 @@ public static class DomainErrors
     public static class Course
     {
         public static readonly Error InvalidCourseUnit = new (
-            "Course.Unit", 
+            StatusCodes.BADREQUEST, 
             "Course unit must be greater than 0 and less than 7");
 
         public static readonly Error CourseNotFound = new(
-            "Course.NotFound",
+            StatusCodes.NOTFOUND,
             "Course not found");
     }
 
     public static class Department
     {
-        public static readonly Error DepartmentNotFound = new("Department.NotFound", "Department not found");
+        public static readonly Error DepartmentNotFound = new
+            (StatusCodes.NOTFOUND, 
+            "Department not found");
     }
 }
