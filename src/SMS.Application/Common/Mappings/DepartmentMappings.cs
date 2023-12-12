924c1c91e1f757d312cef9a98b295e15880023e3
Wale <onafesowale@gmail.com>
2023-12-12 10:50:38 +0100

    department mappings

---
+++ src/SMS.Application/Common/Mappings/DepartmentMappings.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using SMS.Application.Department.Response;
+using DepartmentEntity =  SMS.Domain.Aggregates.DepartmentAggregates.Department;
+
+namespace SMS.Application.Common.Mappings;
+public class DepartmentMappings : Profile
+{
+    public DepartmentMappings()
+    {
+        CreateMap<DepartmentEntity, CreateDepartmentResponse>();
+        CreateMap<DepartmentEntity, GetDepartmentResponse>();
+    }
+}
