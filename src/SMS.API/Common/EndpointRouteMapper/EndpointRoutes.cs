26d06f1a34f6a6b683ce052454f77c6bc5ab817c
Wale <onafesowale@gmail.com>
2023-12-12 18:13:08 +0100

    Endpoint routes

---
+++ src/SMS.API/Common/EndpointRouteMapper/EndpointRoutes.cs
@@ -0,0 +1,27 @@
+﻿using SMS.Application.Courses.Response;
+using SMS.Application.Department.Response;
+
+namespace SMS.API.Common.EndpointRouteMapper;
+
+public static class EndpointRoutes
+{
+    private static Dictionary<Type, string> _mapRoute = new()
+    {
+        {typeof(CreateDepartmentResponse), Names.GETDEPARTMENT },
+        {typeof(CreateCourseResponse), Names.GETCOURSE }
+    };
+
+    public static string GetRouteName(Type type)
+    {
+        if(_mapRoute.ContainsKey(type)) return _mapRoute[type];
+
+        return string.Empty;
+    }
+
+    public static class Names
+    {
+        public const string GETDEPARTMENT = "GetDepartment";
+
+        public const string GETCOURSE = "GetCourse";
+    }
+}
