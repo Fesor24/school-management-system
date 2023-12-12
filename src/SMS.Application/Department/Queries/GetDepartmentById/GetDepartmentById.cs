9153b91d76d441dac862b48c5bfbd20bcad450dc
Wale <onafesowale@gmail.com>
2023-12-12 10:51:45 +0100

    GetDepartmentByIdRequest

---
+++ src/SMS.Application/Department/Queries/GetDepartmentById/GetDepartmentById.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using SMS.Application.Department.Response;
+using SMS.Domain.Errors;
+using SMS.Domain.Primitives;
+using SMS.Domain.Shared;
+
+namespace SMS.Application.Department.Queries.GetDepartmentById;
+public record GetDepartmentByIdRequest(Guid Id) : IRequest<Result<GetDepartmentResponse, Error>>;
+
+internal sealed class GetDepartmentByIdRequestHandler : IRequestHandler<GetDepartmentByIdRequest,
+    Result<GetDepartmentResponse, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetDepartmentByIdRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<GetDepartmentResponse, Error>> Handle(GetDepartmentByIdRequest request, 
+        CancellationToken cancellationToken)
+    {
+        var department = await _unitOfWork.DepartmentRepository.GetAsync(request.Id);
+
+        if (department is null) return Result.Failure<GetDepartmentResponse, Error>(DomainErrors.Department.DepartmentNotFound);
+
+        return _mapper.Map<GetDepartmentResponse>(department);
+    }
+}
