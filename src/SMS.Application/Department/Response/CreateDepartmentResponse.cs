63bf265039df15ddb24b9a6b6f25b96ef0201521
Wale <onafesowale@gmail.com>
2023-12-12 10:52:08 +0100

    CreateDepartmentResponse

---
+++ src/SMS.Application/Department/Response/CreateDepartmentResponse.cs
@@ -0,0 +1,4 @@
+﻿using SMS.Application.Common.Response;
+
+namespace SMS.Application.Department.Response;
+public record CreateDepartmentResponse(Guid Id, string Name, string Code) : CreateResponse(Id);
