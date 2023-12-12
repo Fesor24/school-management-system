5387237573a546822aa1e7de8c8b720a928ba128
Wale <onafesowale@gmail.com>
2023-12-12 10:51:00 +0100

    create course command

--- src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
+++ src/SMS.Application/Courses/Commands/CreateCourse/CreateCourse.cs
@@ -1,23 +1,27 @@
-﻿using MediatR;
-using SMS.Domain.Aggregates.DepartmentAggregates;
-using SMS.Domain.Errors;
-using SMS.Domain.Exceptions.Course;
+﻿using AutoMapper;
+using MediatR;
+using SMS.Application.Courses.Response;
 using SMS.Domain.Exceptions.Department;
 using SMS.Domain.Primitives;
+using SMS.Domain.Shared;
 
 namespace SMS.Application.Courses.Commands.CreateCourse;
-public record CreateCourseCommand(string Name, string Code, int Unit, Guid DepartmentId) : IRequest<Unit>;
+public record CreateCourseCommand(string Name, string Code, int Unit, Guid DepartmentId) : 
+    IRequest<Result<CreateCourseResponse, Error>>;
 
-internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, Unit>
+internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, 
+    Result<CreateCourseResponse, Error>>
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public CreateCourseCommandHandler(IUnitOfWork unitOfWork)
+    public CreateCourseCommandHandler(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
-    public async Task<Unit> Handle(CreateCourseCommand request, 
+    public async Task<Result<CreateCourseResponse, Error>> Handle(CreateCourseCommand request, 
         CancellationToken cancellationToken)
     {
         var department = await _unitOfWork.DepartmentRepository.GetDepartmentInfo(request.DepartmentId, cancellationToken) ??
@@ -25,13 +29,15 @@ internal sealed class CreateCourseCommandHandler : IRequestHandler<CreateCourseC
 
         var result = department.AddCourse(request.Name, request.Code, request.Unit);
 
-        if (result.IsFailure && result.Error.Equals(DomainErrors.Course.InvalidCourseUnit))
-            throw new CourseInvalidUnitException(result.Error.Message);
+        //if (result.IsFailure && result.Error.Equals(DomainErrors.Course.InvalidCourseUnit))
+        //    throw new CourseInvalidUnitException(result.Error.Message);
 
-        Course course = result.Value;
+        if (result.IsFailure) return Result.Failure<CreateCourseResponse, Error>(result.Error);
+
+        //Course course = result.Value;
 
         _unitOfWork.DepartmentRepository.Update(department);
 
-        return Unit.Value;
+        return Result.Success<CreateCourseResponse, Error>(_mapper.Map<CreateCourseResponse>(result));
     }
 }
