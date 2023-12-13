83bd49a8cae7944346d520bcfd7f4cb2016ee753
Wale <onafesowale@gmail.com>
2023-12-13 10:54:12 +0100

    school db context

---
+++ src/SMS.Infrastructure/Data/SchoolDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMS.Domain.Aggregates.DepartmentAggregates;
+using System.Reflection;
+
+namespace SMS.Infrastructure.Data;
+public class SchoolDbContext : DbContext
+{
+    public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options)
+    {
+        
+    }
+
+    internal const string DEFAULT_SCHEMA = "sms";
+
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Department> Departments => Set<Department>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
