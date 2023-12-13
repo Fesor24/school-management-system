6baf427f8e03614edb6e55a507d01897236111a5
Wale <onafesowale@gmail.com>
2023-12-13 10:51:25 +0100

    delete department command

---
+++ src/SMS.Application/Department/Commands/DeleteDepartment/DeleteDepartment.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using SMS.Domain.Errors;
+using SMS.Domain.Primitives;
+using SMS.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS.Application.Department.Commands.DeleteDepartment;
+public record DeleteDepartmentCommand(Guid Id) : IRequest<Result<Unit, Error>>;
+
+internal sealed class DeleteDepartmentCommandHandler : IRequestHandler<DeleteDepartmentCommand, Result<Unit, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteDepartmentCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<Unit, Error>> Handle(DeleteDepartmentCommand request, CancellationToken cancellationToken)
+    {
+        var department = await _unitOfWork.DepartmentRepository.GetAsync(request.Id);
+
+        if (department is null) return DomainErrors.Department.DepartmentNotFound(request.Id);
+
+        _unitOfWork.DepartmentRepository.Delete(department);
+
+        await _unitOfWork.Complete();
+
+        return Unit.Value;
+    }
+}
