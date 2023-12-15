d7b931dfa8a81dafcf8be464d5ff48a647e2788e
Wale <onafesowale@gmail.com>
2023-12-15 09:35:57 +0100

    Create department validator

---
+++ src/SMS.Application/Department/Commands/CreateDepartment/CreateDepartmentValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace SMS.Application.Department.Commands.CreateDepartment;
+public class CreateDepartmentValidator : AbstractValidator<CreateDepartmentCommand>
+{
+    public CreateDepartmentValidator()
+    {
+        RuleFor(x => x.Code)
+            .NotEmpty().WithMessage("Code can not be empty")
+            .NotNull().WithMessage("Code can not be null");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name can not be empty")
+            .NotNull().WithMessage("Name can not be null");
+    }
+}
