ce7b2545603fe9afa9c7b1c9dafbd0f0f5c92178
Wale <onafesowale@gmail.com>
2023-12-15 09:37:06 +0100

    added method to repo

--- src/SMS.Domain/Aggregates/DepartmentAggregates/IDepartmentRepository.cs
+++ src/SMS.Domain/Aggregates/DepartmentAggregates/IDepartmentRepository.cs
@@ -6,4 +6,6 @@ public interface IDepartmentRepository : IGenericRepository<Department>
     Task<Department?> GetDepartmentInfo(Guid departmentId, CancellationToken cancellationtoken = default);
 
     Task<Course?> GetCourseAsync(Guid courseId, CancellationToken cancellationToken = default);
+
+    Task AddCourseAsync(Course coure, CancellationToken cancellationToken = default);
 }
