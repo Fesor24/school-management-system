8125c35434048580ca85ff2aa465bac7c06539c8
Wale <onafesowale@gmail.com>
2023-12-15 09:34:47 +0100

    Added mappings

--- src/SMS.Application/Common/Mappings/CourseMappings.cs
+++ src/SMS.Application/Common/Mappings/CourseMappings.cs
@@ -1,18 +1,20 @@
 ﻿using AutoMapper;
-using SMS.Application.Common.Response;
+using SMS.Application.Courses.Request;
 using SMS.Application.Courses.Response;
 using SMS.Domain.Aggregates.DepartmentAggregates;
+using SMS.Domain.Models.Course;
 
 namespace SMS.Application.Common.Mappings;
 internal class CourseMappings : Profile
 {
     public CourseMappings()
     {
-        CreateMap<Course, GetCourseResponse>();
-        //CreateMap<Course, CreateCourseResponse>()
-        //    .ForMember(x => x.Code, o => o.MapFrom(s => s.CourseInfo.Code))
-        //    .ForMember(x => x.Name, o => o.MapFrom(s => s.CourseInfo.Name));
+        CreateMap<Course, GetCourseResponse>()
+            .ConstructUsing(x => new(x.Id, x.CourseInfo.Name, x.CourseInfo.Code, x.Unit));
+
         CreateMap<Course, CreateCourseResponse>()
             .ConstructUsing(x => new CreateCourseResponse(x.Id, x.CourseInfo.Name, x.CourseInfo.Code, x.Unit));
+
+        CreateMap<CreateCourseRequest, CourseModel>();
     }
 }
