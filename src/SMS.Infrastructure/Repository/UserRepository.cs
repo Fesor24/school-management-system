db20a5773b66fb74993487994310448d0142b970
Wale <onafesowale@gmail.com>
2023-12-16 14:31:36 +0100

    User repository

---
+++ src/SMS.Infrastructure/Repository/UserRepository.cs
@@ -0,0 +1,11 @@
+﻿using SMS.Domain.Aggregates.UserAggregates;
+using SMS.Infrastructure.Data;
+
+namespace SMS.Infrastructure.Repository;
+public class UserRepository : GenericRepository<User>, IUserRepository
+{
+    public UserRepository(SchoolDbContext context) : base(context)
+    {
+        
+    }
+}
