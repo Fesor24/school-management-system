0907a180e07eca610b62095fa6cb05f3f4666758
Wale <onafesowale@gmail.com>
2023-12-16 14:31:02 +0100

    Address value object

---
+++ src/SMS.Domain/Aggregates/UserAggregates/Address.cs
@@ -0,0 +1,23 @@
+﻿using SMS.Domain.Primitives;
+
+namespace SMS.Domain.Aggregates.UserAggregates;
+public sealed class Address : ValueObject
+{
+    public Address(string street, string city, string state)
+    {
+        Street = street;
+        City = city;
+        State = state;
+    }
+
+    public string Street { get; private set; }
+    public string City { get; private set; }
+    public string State { get; private set; }
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Street;
+        yield return City;
+        yield return State;
+    }
+}
