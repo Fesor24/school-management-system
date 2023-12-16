7c13f27ed0b91f000fff4258623543a4d7845b0e
Wale <onafesowale@gmail.com>
2023-12-16 14:31:15 +0100

    Gender enum

---
+++ src/SMS.Domain/Aggregates/UserAggregates/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace SMS.Domain.Aggregates.UserAggregates;
+public enum Gender
+{
+    [EnumMember(Value = "Unspecified")]
+    Unspecified,
+    [EnumMember(Value = "Male")]
+    Male,
+    [EnumMember(Value = "Female")]
+    Female,
+}
