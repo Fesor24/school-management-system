61a7a8aa6ece8d1365e3b3871c61860797adf64a
Wale <onafesowale@gmail.com>
2023-12-17 01:53:53 +0100

    unprocessable entity exception

---
+++ src/SMS.Domain/Exceptions/BaseExceptions/UnprocessableEntityException.cs
@@ -0,0 +1,9 @@
+﻿namespace SMS.Domain.Exceptions.BaseExceptions;
+public class UnprocessableEntityException : Exception
+{
+    public IReadOnlyDictionary<string, string[]> Errors { get;}
+
+    public UnprocessableEntityException(IReadOnlyDictionary<string, string[]> errors) : 
+        base("One or more validation error(s)") =>
+        Errors = errors;
+}
