69232ecf86c2097631da4f4d2fb2ce146a51d89c
Wale <onafesowale@gmail.com>
2023-12-17 01:53:19 +0100

    update course validator

---
+++ src/SMS.Application/Courses/Commands/UpdateCourse/UpdateCourseValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace SMS.Application.Courses.Commands.UpdateCourse;
+public sealed class UpdateCourseValidator : AbstractValidator<UpdateCourseCommand>
+{
+    public UpdateCourseValidator()
+    {
+        RuleFor(x => x.Code)
+            .NotEmpty().WithMessage("Code can not be empty")
+            .NotNull().WithMessage("Code can not be null");
+
+        RuleFor(x => x.Name)
+           .NotEmpty().WithMessage("Name can not be empty")
+           .NotNull().WithMessage("Name can not be null");
+
+        RuleFor(x => x.Unit)
+           .LessThanOrEqualTo(6).WithMessage("Unit can not be more than 6")
+           .GreaterThanOrEqualTo(1).WithMessage("Unit must be greater than 0");
+    }
+}
