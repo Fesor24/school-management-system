d55663d118c4dec9f69b92aafbc561d4105ffd23
Wale <onafesowale@gmail.com>
2023-12-17 01:53:30 +0100

    update department validator

---
+++ src/SMS.Application/Department/Commands/UpdateDepartment/UpdateDepartmentValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace SMS.Application.Department.Commands.UpdateDepartment;
+public sealed class UpdateDepartmentValidator : AbstractValidator<UpdateDepartmentCommand>
+{
+    public UpdateDepartmentValidator()
+    {
+        RuleFor(x => x.Code)
+            .NotEmpty().WithMessage("Code can not be empty")
+            .NotNull().WithMessage("Code can not be null");
+
+        RuleFor(x => x.Name)
+           .NotEmpty().WithMessage("Name can not be empty")
+           .NotNull().WithMessage("Name can not be null");
+    }
+}
