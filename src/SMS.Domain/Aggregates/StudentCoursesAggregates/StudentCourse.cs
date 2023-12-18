b75f4398720a499f026a399321f9f836af25b44c
Wale <onafesowale@gmail.com>
2023-12-18 18:42:23 +0100

    student course entity

---
+++ src/SMS.Domain/Aggregates/StudentCoursesAggregates/StudentCourse.cs
@@ -0,0 +1,23 @@
+﻿using SMS.Domain.Aggregates.DepartmentAggregates;
+using SMS.Domain.Aggregates.StudentAggregates;
+using SMS.Domain.Primitives;
+
+namespace SMS.Domain.Aggregates.StudentCoursesAggregates;
+public sealed class StudentCourse : AggregateRoot
+{
+    public StudentCourse()
+    {
+        
+    }
+
+    public StudentCourse(Guid studentId, Guid courseId)
+    {
+        StudentId = studentId;
+        CourseId = courseId;
+    }
+
+    public Guid StudentId { get; private set; }
+    public Guid CourseId { get; private set; }
+    public Course Course { get; private set; }
+    public Student Student { get; private set; }
+}
