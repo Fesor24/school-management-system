343ca1ad40dcb65f7aebb39f3a3b18c87a143fba
Wale <onafesowale@gmail.com>
2023-12-18 18:44:33 +0100

    use department repository

--- src/SMS.Application/Courses/Queries/GetCourseById/GetCourseById.cs
+++ src/SMS.Application/Courses/Queries/GetCourseById/GetCourseById.cs
@@ -1,8 +1,8 @@
 ﻿using AutoMapper;
 using MediatR;
 using SMS.Application.Courses.Response;
+using SMS.Domain.Aggregates.DepartmentAggregates;
 using SMS.Domain.Errors;
-using SMS.Domain.Primitives;
 using SMS.Domain.Shared;
 
 namespace SMS.Application.Courses.Queries.GetCourseById;
@@ -11,19 +11,19 @@ public record GetCourseByIdRequest(Guid Id) : IRequest<Result<GetCourseResponse,
 internal sealed class GetCourseByIdRequestHandler : IRequestHandler<GetCourseByIdRequest, 
     Result<GetCourseResponse, Error>>
 {
-    private readonly IUnitOfWork _unitOfWork;
+    private readonly IDepartmentRepository _departmentRepository;
     private readonly IMapper _mapper;
 
-    public GetCourseByIdRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public GetCourseByIdRequestHandler(IDepartmentRepository departmentRepository, IMapper mapper)
     {
-        _unitOfWork = unitOfWork;
+        _departmentRepository = departmentRepository;
         _mapper = mapper;
     }
 
     public async Task<Result<GetCourseResponse, Error>> Handle(GetCourseByIdRequest request, 
         CancellationToken cancellationToken)
     {
-        var course = await _unitOfWork.DepartmentRepository.GetCourseAsync(request.Id, cancellationToken);
+        var course = await _departmentRepository.GetCourseAsync(request.Id, cancellationToken);
 
         if (course is null) return DomainErrors.Course.CourseNotFound(request.Id);
 
