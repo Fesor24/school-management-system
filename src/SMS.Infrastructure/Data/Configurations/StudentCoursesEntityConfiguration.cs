4a6a5078135021aaa2f4af39a014975d40ecd48f
Wale <onafesowale@gmail.com>
2023-12-18 18:41:52 +0100

    student course entity configuration

---
+++ src/SMS.Infrastructure/Data/Configurations/StudentCoursesEntityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SMS.Domain.Aggregates.StudentCoursesAggregates;
+
+namespace SMS.Infrastructure.Data.Configurations;
+public class StudentCoursesEntityConfiguration : IEntityTypeConfiguration<StudentCourse>
+{
+    public void Configure(EntityTypeBuilder<StudentCourse> builder)
+    {
+        builder.ToTable(nameof(StudentCourse), SchoolDbContext.STUDENT_SCHEMA);
+
+        builder.HasKey(sc => new { sc.StudentId, sc.CourseId });
+
+        builder.Ignore(x => x.Id);
+
+        builder.Ignore(x => x.DomainEvents);
+    }
+}
