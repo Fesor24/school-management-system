8e95da3391454cb6cd901672c42c368bf4094260
Wale <onafesowale@gmail.com>
2023-12-18 18:43:02 +0100

    student entity

---
+++ src/SMS.Domain/Aggregates/StudentAggregates/Student.cs
@@ -0,0 +1,27 @@
+﻿using SMS.Domain.Aggregates.StudentCoursesAggregates;
+using SMS.Domain.Primitives;
+
+namespace SMS.Domain.Aggregates.StudentAggregates;
+public sealed class Student : AggregateRoot
+{
+    private readonly List<StudentCourse> _courses = new();
+
+    public Student()
+    {
+        
+    }
+
+    public Student(Guid id, Guid userId, Guid departmentId) : base(id)
+    {
+        UserId = userId;
+        DepartmentId = departmentId;
+    }
+
+    public decimal GCPA { get; private set; } = 0m;
+
+    public Guid UserId { get;private set; }
+
+    public Guid DepartmentId { get; private set; }
+
+    public IReadOnlyCollection<StudentCourse> Courses => _courses.AsReadOnly();
+}
