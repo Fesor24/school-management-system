fc49d1dd14fed9e2e5d69aa1b08650b55c707421
Wale <onafesowale@gmail.com>
2023-12-18 18:43:57 +0100

    use department repository

--- src/SMS.Application/Department/Queries/GetDepartments/GetDepartments.cs
+++ src/SMS.Application/Department/Queries/GetDepartments/GetDepartments.cs
@@ -1,13 +1,8 @@
 ﻿using AutoMapper;
 using MediatR;
 using SMS.Application.Department.Response;
-using SMS.Domain.Primitives;
+using SMS.Domain.Aggregates.DepartmentAggregates;
 using SMS.Domain.Shared;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SMS.Application.Department.Queries.GetDepartments;
 public record GetDepartmentsRequest() : IRequest<Result<IReadOnlyList<GetDepartmentResponse>, Error>>;
@@ -15,19 +10,19 @@ public record GetDepartmentsRequest() : IRequest<Result<IReadOnlyList<GetDepartm
 internal sealed class GetDepartmentsRequestHandler : IRequestHandler<GetDepartmentsRequest,
     Result<IReadOnlyList<GetDepartmentResponse>, Error>>
 {
-    private readonly IUnitOfWork _unitOfWork;
+    private readonly IDepartmentRepository _departmentRepository;
     private readonly IMapper _mapper;
 
-    public GetDepartmentsRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    public GetDepartmentsRequestHandler(IDepartmentRepository departmentRepository, IMapper mapper)
     {
-        _unitOfWork = unitOfWork;
+        _departmentRepository = departmentRepository;
         _mapper = mapper;
     }
 
     public async Task<Result<IReadOnlyList<GetDepartmentResponse>, Error>> Handle(GetDepartmentsRequest request, 
         CancellationToken cancellationToken)
     {
-        var departments = await _unitOfWork.DepartmentRepository.GetAll(cancellationToken);
+        var departments = await _departmentRepository.GetAll(cancellationToken);
 
         return Result.Success<IReadOnlyList<GetDepartmentResponse>, Error>(
             _mapper.Map<IReadOnlyList<GetDepartmentResponse>>(departments));
