6ff552aa2c7e2c6d9f894223f6a972466562e71d
Wale <onafesowale@gmail.com>
2023-12-18 18:41:19 +0100

    unit of work

--- src/SMS.Infrastructure/Data/SchoolDbContext.cs
+++ src/SMS.Infrastructure/Data/SchoolDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SMS.Domain.Aggregates.DepartmentAggregates;
+using SMS.Domain.Primitives;
 using System.Reflection;
 
 namespace SMS.Infrastructure.Data;
-public class SchoolDbContext : DbContext
+public class SchoolDbContext : DbContext, IUnitOfWork
 {
     public SchoolDbContext(DbContextOptions<SchoolDbContext> options) : base(options)
     {
@@ -12,6 +13,7 @@ public class SchoolDbContext : DbContext
 
     internal const string DEFAULT_SCHEMA = "sms";
     internal const string USER_SCHEMA = "usr";
+    internal const string STUDENT_SCHEMA = "std";
 
     public DbSet<Course> Courses => Set<Course>();
     public DbSet<Department> Departments => Set<Department>();
@@ -21,5 +23,10 @@ public class SchoolDbContext : DbContext
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public async Task<int> SaveEntitiesAsync(CancellationToken cancelleationToken = default)
+    {
+        return await base.SaveChangesAsync();
     }
 }
