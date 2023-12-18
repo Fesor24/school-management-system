2ed9134d4e271e3d6977ef57e0dc06f19743b095
Wale <onafesowale@gmail.com>
2023-12-18 18:41:37 +0100

    student entity configuration

---
+++ src/SMS.Infrastructure/Data/Configurations/StudentEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SMS.Domain.Aggregates.StudentAggregates;
+
+namespace SMS.Infrastructure.Data.Configurations;
+public class StudentEntityConfiguration : IEntityTypeConfiguration<Student>
+{
+    public void Configure(EntityTypeBuilder<Student> builder)
+    {
+        builder.ToTable(nameof(Student), SchoolDbContext.STUDENT_SCHEMA);
+
+        builder.HasKey(x => x.Id);
+
+        builder.Ignore(x => x.DomainEvents);
+
+        var navigation = builder.Metadata.FindNavigation(nameof(Student.Courses));
+
+        navigation.SetPropertyAccessMode(PropertyAccessMode.Field);
+    }
+}
