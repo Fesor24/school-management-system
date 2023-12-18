e20d881b02a626e575aa0b37aca349a8d6fdb07d
Wale <onafesowale@gmail.com>
2023-12-18 18:39:41 +0100

    student course repository

---
+++ src/SMS.Infrastructure/Repository/StudentCourseRepository.cs
@@ -0,0 +1,11 @@
+﻿using SMS.Domain.Aggregates.StudentCoursesAggregates;
+using SMS.Infrastructure.Data;
+
+namespace SMS.Infrastructure.Repository;
+public sealed class StudentCourseRepository : GenericRepository<StudentCourse>, IStudentCourseRepository
+{
+    public StudentCourseRepository(SchoolDbContext context) : base(context)
+    {
+        
+    }
+}
