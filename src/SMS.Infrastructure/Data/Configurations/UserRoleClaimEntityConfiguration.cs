0e2662d846806710153ed014a924fbfa15384120
Wale <onafesowale@gmail.com>
2024-01-02 13:13:29 +0100

    entity configuration

---
+++ src/SMS.Infrastructure/Data/Configurations/UserRoleClaimEntityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SMS.Domain.Aggregates.UserRoleClalimAggregates;
+
+namespace SMS.Infrastructure.Data.Configurations;
+internal sealed class UserRoleClaimEntityConfiguration : IEntityTypeConfiguration<UserRoleClaim>
+{
+    public void Configure(EntityTypeBuilder<UserRoleClaim> builder)
+    {
+        builder.ToTable(nameof(UserRoleClaim), SchoolDbContext.USER_SCHEMA);
+    }
+}
