a20c232199ec785723329227e8cab22564b2b110
Wale <onafesowale@gmail.com>
2024-01-02 13:12:01 +0100

    User role claim entity

---
+++ src/SMS.Domain/Aggregates/UserRoleClalimAggregates/UserRoleClaim.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SMS.Domain.Aggregates.UserRoleClalimAggregates;
+public sealed class UserRoleClaim : IdentityRoleClaim<Guid>
+{
+    public UserRoleClaim()
+    {
+        
+    }
+
+    public UserRoleClaim(string description, string group)
+    {
+        Description = description;
+        Group = group;
+    }
+
+    public string Description { get; private set; }
+    public string Group { get; private set; }
+}
