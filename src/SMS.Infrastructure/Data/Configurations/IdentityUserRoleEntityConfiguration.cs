8b9bd35873b05c80c863d88b8034703b180075a7
Wale <onafesowale@gmail.com>
2024-01-02 13:13:06 +0100

    entity configuration

---
+++ src/SMS.Infrastructure/Data/Configurations/IdentityUserRoleEntityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SMS.Infrastructure.Data.Configurations;
+internal sealed class IdentityUserRoleEntityConfiguration : IEntityTypeConfiguration<IdentityUserRole<Guid>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserRole<Guid>> builder)
+    {
+        builder.ToTable("UserRole", SchoolDbContext.USER_SCHEMA);
+
+    }
+}
