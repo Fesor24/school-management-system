de9f10458a366a637efa70f7d1c1e9affa9f4d2d
Wale <onafesowale@gmail.com>
2024-01-02 13:13:10 +0100

    entity configuration

---
+++ src/SMS.Infrastructure/Data/Configurations/IdentityUserTokenEntityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SMS.Infrastructure.Data.Configurations;
+internal sealed class IdentityUserTokenEntityConfiguration : IEntityTypeConfiguration<IdentityUserToken<Guid>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserToken<Guid>> builder)
+    {
+        builder.ToTable("UserToken", SchoolDbContext.USER_SCHEMA);
+    }
+}
