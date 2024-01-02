9abd0be8cba9dc1d497f3cf924d68ba257610a12
Wale <onafesowale@gmail.com>
2024-01-02 13:11:49 +0100

    user role entity

---
+++ src/SMS.Domain/Aggregates/UserRoleAggregates/UserRole.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SMS.Domain.Primitives;
+
+namespace SMS.Domain.Aggregates.UserRoleAggregates;
+public sealed class UserRole : IdentityRole<Guid>, IEntity
+{
+    public UserRole()
+    {
+        
+    }
+
+    public UserRole(string description, string name) : base(name)
+    {
+        Description = description;
+    }
+    public string Description { get; private set; }
+}
