36c8dbd4e7577109577590db5667b331b3ee3443
Wale <onafesowale@gmail.com>
2024-01-02 13:13:02 +0100

    entity configuration

---
+++ src/SMS.Infrastructure/Data/Configurations/IdentityUserLoginEntityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SMS.Infrastructure.Data.Configurations;
+internal sealed class IdentityUserLoginEntityConfiguration : IEntityTypeConfiguration<IdentityUserLogin<Guid>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserLogin<Guid>> builder)
+    {
+        builder.ToTable("UserLogin", SchoolDbContext.USER_SCHEMA);
+    }
+}
