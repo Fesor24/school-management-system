6f2e4cfc13e98b7702375b6fbb512a07d80e2063
Wale <onafesowale@gmail.com>
2024-01-02 13:12:58 +0100

    entity configuration

---
+++ src/SMS.Infrastructure/Data/Configurations/IdentityUserClaimEntityConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SMS.Infrastructure.Data.Configurations;
+internal sealed class IdentityUserClaimEntityConfiguration : IEntityTypeConfiguration<IdentityUserClaim<Guid>>
+{
+    public void Configure(EntityTypeBuilder<IdentityUserClaim<Guid>> builder)
+    {
+        builder.ToTable("UserClaim", SchoolDbContext.USER_SCHEMA);
+    }
+}
