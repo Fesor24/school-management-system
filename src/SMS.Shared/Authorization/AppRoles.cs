5aab76fab7ec118693bc4cf0900a42e545296f55
Wale <onafesowale@gmail.com>
2024-01-04 08:43:45 +0100

    App roles

---
+++ src/SMS.Shared/Authorization/AppRoles.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace SMS.Shared.Authorization;
+public static class AppRoles
+{
+    public const string Admin = nameof(Admin);
+    public const string User = nameof(User);
+
+    public static IReadOnlyList<string> DefaultRoles { get; } = new ReadOnlyCollection<string>(new[]
+    {
+        Admin,
+        User,
+    });
+
+    public static bool IsDefault(string roleName) => 
+        DefaultRoles.Contains(roleName);
+}
