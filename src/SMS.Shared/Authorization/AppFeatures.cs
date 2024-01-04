0fda18727860b72b543a6eea963d0ae047b1207a
Wale <onafesowale@gmail.com>
2024-01-04 08:43:24 +0100

    App features

---
+++ src/SMS.Shared/Authorization/AppFeatures.cs
@@ -0,0 +1,9 @@
+﻿namespace SMS.Shared.Authorization;
+public static class AppFeatures
+{
+    public const string Student = nameof(Student);
+    public const string User = nameof(User);
+    public const string Role = nameof(Role);
+    public const string UserRoles = nameof(UserRoles);
+    public const string RoleClaims = nameof(RoleClaims);
+}
