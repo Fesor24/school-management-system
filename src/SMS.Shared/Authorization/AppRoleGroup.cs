d7e851e151f3235f0296e6b920781af0eab3fa90
Wale <onafesowale@gmail.com>
2024-01-04 08:43:37 +0100

    App role group

---
+++ src/SMS.Shared/Authorization/AppRoleGroup.cs
@@ -0,0 +1,6 @@
+﻿namespace SMS.Shared.Authorization;
+public static class AppRoleGroup
+{
+    public const string SystemAccess = nameof(SystemAccess);
+    public const string Management = nameof(Management);
+}
