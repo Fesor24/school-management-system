8ec7b450017e48b1f64dc78a05c3691d23cae63e
Wale <onafesowale@gmail.com>
2024-01-04 08:43:09 +0100

    App actions

---
+++ src/SMS.Shared/Authorization/AppActions.cs
@@ -0,0 +1,8 @@
+﻿namespace SMS.Shared.Authorization;
+public static class AppActions
+{
+    public const string Create = nameof(Create);
+    public const string Read = nameof(Read);
+    public const string Update = nameof(Update);
+    public const string Delete = nameof(Delete);
+}
