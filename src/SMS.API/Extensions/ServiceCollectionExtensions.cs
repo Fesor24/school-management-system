1c10ca6fef999600b9abe9a74f009971a56ca3d0
Wale <onafesowale@gmail.com>
2024-01-25 13:46:59 +0100

    api service extensions

---
+++ src/SMS.API/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using SMS.API.Permissions;
+
+namespace SMS.API.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IAuthorizationPolicyProvider, PermissionPolicyProvider>();
+
+        services.AddScoped<IAuthorizationHandler, PermissionAuthorizationHandler>();
+
+        return services;
+    }
+}
