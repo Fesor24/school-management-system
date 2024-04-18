49f2070eeddbcbbaf6e37645419501f25028f111
Wale <onafesowale@gmail.com>
2024-04-18 09:30:26 +0100

    add error responses

--- src/SMS.Domain/Errors/DomainErrors.cs
+++ src/SMS.Domain/Errors/DomainErrors.cs
@@ -5,23 +5,23 @@ public static class DomainErrors
 {
     public static class Course
     {
-        public static readonly Error InvalidCourseUnit = new (
-            StatusCodes.BADREQUEST, 
+        public static readonly Error InvalidCourseUnit = Error.Validation(
+            "COURSE.INVALIDUNIT", 
             "Course unit must be greater than 0 and less than 7");
 
-        public static Error CourseNotFound(Guid courseId) => new(
-            StatusCodes.NOTFOUND,
+        public static Error CourseNotFound(Guid courseId) => Error.NotFound(
+            "COURSE.NOTFOUND",
             $"Course with Id: '{courseId}' not found");
     }
 
     public static class Department
     {
-        public static Error DepartmentNotFound(Guid departmentId) => new
-            (StatusCodes.NOTFOUND, 
+        public static Error DepartmentNotFound(Guid departmentId) => Error.NotFound
+            ("DEPARTMENT.NOTFOUND", 
             $"Department with Id: '{departmentId}' not found");
 
-        public static Error DepartmentBadRequest(params string[] data) => new Error(
-            StatusCodes.BADREQUEST,
+        public static Error DepartmentBadRequest(params string[] data) => Error.Validation(
+            "INVALID.INPUT",
             $"Ensure validity of data passed: {data}"
             );
     }
