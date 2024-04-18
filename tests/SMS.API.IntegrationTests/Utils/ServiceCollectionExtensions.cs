1be49e49016f8b1512722606af3c277582c37daa
Wale <onafesowale@gmail.com>
2024-04-18 20:54:26 +0100

    service collection extensions

---
+++ tests/SMS.API.IntegrationTests/Utils/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SMS.Infrastructure.Data;
+
+namespace SMS.API.IntegrationTests.Utils;
+public static class ServiceCollectionExtensions
+{
+    public static void RemoveDbContext<TContext>(this IServiceCollection services)
+    {
+        var dbDescriptor = services.SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<SchoolDbContext>));
+
+        if(dbDescriptor is not null)
+            services.Remove(dbDescriptor);
+    }
+
+    public static void DbContextInit<TContext>(this IServiceCollection services)
+    {
+        var serviceProvider = services.BuildServiceProvider();
+
+        using var scope = serviceProvider.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<SchoolDbContext>();
+
+        context.Database.Migrate();
+    }
+}
