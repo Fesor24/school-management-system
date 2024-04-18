1e7fa4884f2e1ab8149cf161abeaac4541b1b056
Wale <onafesowale@gmail.com>
2024-04-18 20:54:06 +0100

    CustomApplicationFactory

---
+++ tests/SMS.API.IntegrationTests/Fixtures/CustomApplicationFactory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SMS.API.IntegrationTests.Utils;
+using SMS.Infrastructure.Data;
+using Testcontainers.PostgreSql;
+
+namespace SMS.API.IntegrationTests.Fixtures;
+public class CustomApplicationFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
+{
+    private readonly PostgreSqlContainer _container = new PostgreSqlBuilder().Build();
+
+    public async Task InitializeAsync()
+    {
+        await _container.StartAsync();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveDbContext<SchoolDbContext>();
+
+            services.AddDbContext<SchoolDbContext>(opt =>
+            {
+                opt.UseNpgsql(_container.GetConnectionString());
+            });
+
+            services.DbContextInit<SchoolDbContext>();
+        });
+    }
+
+    public new async Task DisposeAsync()
+    {
+        await _container.DisposeAsync();
+    }
+}
