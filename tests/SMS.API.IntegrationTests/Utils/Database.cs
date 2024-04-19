8dc1330aef1b900361e79a2bd3d4464abb1e12fe
Wale <onafesowale@gmail.com>
2024-04-19 05:18:11 +0100

    Database class

---
+++ tests/SMS.API.IntegrationTests/Utils/Database.cs
@@ -0,0 +1,21 @@
+﻿using Bogus;
+using SMS.Domain.Aggregates.DepartmentAggregates;
+using SMS.Infrastructure.Data;
+
+namespace SMS.API.IntegrationTests.Utils;
+internal static class Database
+{
+    private static readonly Faker<Department> _departmentGenerator = new Faker<Department>()
+        .RuleFor(x => x.Id, faker => Guid.NewGuid())
+        .RuleFor(x => x.Name, faker => faker.Lorem.Letter(10))
+        .RuleFor(x => x.Code, faker => faker.Lorem.Letter(3));
+
+    internal static void Initialize(SchoolDbContext context)
+    {
+        List<Department> departments = _departmentGenerator.GenerateBetween(2, 6);
+
+        context.Departments.AddRange(departments);
+
+        context.SaveChanges();
+    }
+}
